e05d11912e3b8e58314caafd31e92b3f90aff079
GO_RAE <57890052+jun-gyu@users.noreply.github.com>
2020-10-20 15:49:22 +0900

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://leehyojin:leehyojin7202@cluster0.v5o9k.mongodb.net/BookSquare?retryWrites=true&w=majority",
-  secretOrKey: "leehyojin",
-  PORT: 3002,
-  ClientId: "NmrHy8iZGfvlPY8RYnzL",
-  ClientSecret: "7VX0VDQzLG",
-};
